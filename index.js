08efe670c60af403fc833b8109e9512638213b22
Igor Ferreira <48239402+imagno@users.noreply.github.com>
2022-04-05 19:08:59 -0300

    criada primeira rota

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`servidor rodando na porta ${port}`);
 });
+
+app.get('/atendimentos', (_, res) => {
+  res.status(200).send('Você está na rota de atendimentos.');
+});
