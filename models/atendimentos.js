3afd8e49a94e7baca387ce643b73aee0df69da19
Igor Ferreira <48239402+imagno@users.noreply.github.com>
2022-04-07 21:32:14 -0300

    validacoes de seguranca

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,21 +5,47 @@ class Atendimento {
   adiciona(atendimento, res) {
     const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
     const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
-    const atendimentoDatado = {...atendimento, dataCriacao, data};
-
-    const sql = 'INSERT INTO atendimentos SET ?'
     
-    conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-      if(erro) {
-        res
-          .status(400)
-          .json(erro);
-      } else {
-        res
-          .status(201)
-          .json(resultados);
+    const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+    const clienteEhValido = atendimento.cliente.length >= 4;
+    
+    const validacoes = [
+      {
+        nome: 'data',
+        valido: dataEhValida,
+        mensagem: 'Data deve ser maior ou igual a data atual'
+      },
+      {
+        nome: 'cliente',
+        valido: clienteEhValido,
+        mensagem: 'Cliente deve ter pelo menos quatro caracteres'
       }
-    });
+    ];
+
+    const erros = validacoes.filter(campo => !campo.valido);
+    const existemErros = erros.length > 0;
+
+    if(existemErros) {
+      res
+        .status(400)
+        .json(erros);
+    } else {
+      const atendimentoDatado = {...atendimento, dataCriacao, data};
+
+      const sql = 'INSERT INTO atendimentos SET ?'
+    
+      conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+        if(erro) {
+          res
+            .status(400)
+            .json(erro);
+        } else {
+          res
+            .status(201)
+            .json(resultados);
+        }
+      });
+    }
   }
 }
 
