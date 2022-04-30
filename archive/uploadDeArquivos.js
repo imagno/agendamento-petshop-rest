791592052b4d61c16eff46e49d5b6ef5b8d644d3
Igor Ferreira <48239402+imagno@users.noreply.github.com>
2022-04-29 21:16:01 -0300

    implementando seguranca e tratamento de erros no recebimento de imagens

--- archive/uploadDeArquivos.js
+++ archive/uploadDeArquivos.js
@@ -1,9 +1,20 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
-  const novoCaminho = `../assets/images/${nomeDoArquivo}`;
+  const tiposValidos = ['jpg', 'png', 'jpeg'];
+  const tipo = path.extname(caminho);
+  const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) !== -1;
+
+  if(tipoEhValido) {
+    const erro = 'Tipo é inválido';
+    console.log('Erro! Tipo inválido');
+    callbackImagemCriada(erro);
+  } else {
+    const novoCaminho = `../assets/images/${nomeDoArquivo}${tipo}`;
   
-  fs.createReadStream(caminho)
-    .pipe(fs.createWriteStream(novoCaminho))
-    .on('finish', () => callbackImagemCriada(novoCaminho))
+    fs.createReadStream(caminho)
+      .pipe(fs.createWriteStream(novoCaminho))
+      .on('finish', () => callbackImagemCriada(false, novoCaminho));
+  }
 }
--- models/pets.js
+++ models/pets.js
@@ -5,17 +5,21 @@ class Pet {
   adiciona(pet, res) {
     const query = 'INSERT INTO pets SET ?';
 
-    uploadDeArquivo(pet.imagem, pet.nome, (novoCaminho, ) => {
-      const novoPet = { nome: pet.nome, imagem: novoCaminho}
+    uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho, ) => {
+      if(erro) {
+        const novoPet = { nome: pet.nome, imagem: novoCaminho}
 
-      conexao.query(query, novoPet, erro => {
-        if(erro) {
-          console.log(erro);
-          res.status(400).json(erro);
-        } else {
-          res.status(200).json(novoPet);
-        }
-      });
+        conexao.query(query, novoPet, erro => {
+          if(erro) {
+            console.log(erro);
+            res.status(400).json(erro);
+          } else {
+            res.status(200).json(novoPet);
+          }
+        });
+      } else {
+        res.status(400).json({ erro });
+      }
     });
   }
 }
